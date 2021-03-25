a216b989243a5ddbbe15914c6782cc49a49998ec
Jehazekel <jeremiahstrong321@gmail.com>
2021-03-25 01:39:48 +0000

    commit

--- App/main.py
+++ App/main.py
@@ -40,6 +40,9 @@ jwt = JWT(app, authenticate, identity)
 
 ''' End JWT Setup '''
 
+
+
+
 # edit to query 50 pokemon objects and send to template
 @app.route('/', methods=['GET'])
 def index():
@@ -66,6 +69,88 @@ def signup():
         return 'username or email already exists' # error message
     return 'user created' # success
 
+
+@app.route('/mypokemon', methods=["POST"])
+@jwt_required()
+def save_mypokemon():
+    data= request.get_json()    #get the user name and pokemon id 
+    user= User.query.filter_by(username=data['name']).first()
+    if user!= None:
+        user= user.toDict()
+        pokemon= Pokemon.query.get(data['pid'])
+        mypokemon= MyPokemon(pid=data['pid'], id=user['id'], name= user['username'],pokemon= pokemon)
+        db.session.add(mypokemon)
+        db.session.commit()
+        return data['name'] + " captured"
+    return "No Pokemon captured!"
+
+
+@app.route('/mypokemon', methods=["GET"])
+@jwt_required()
+def list_mypokemon():
+    username = current_identity.username
+    user= User.query.filter_by(username= username).first()
+    if user:
+        user_id= user.id
+        user_pokemons= MyPokemon.query.filter_by(id= user_id)
+        user_pokemons= [pokemon.toDict() for pokemon in user_pokemons]
+        return json.dumps( user_pokemons )
+    return "No Pokemon captured!"
+
+
+@app.route('/mypokemon/<user_nth_pokemon>', methods=["GET"])
+@jwt_required()
+def get_mypokemon(user_nth_pokemon):
+    username= current_identity.username
+    user= User.query.filter_by(username= username).first()
+    if user:
+        user_id= user.id
+        all_user_pokemons= MyPokemon.query.filter_by(id= user_id)
+        all_user_pokemons= [pokemon.toDict() for pokemon in all_user_pokemons]
+        if int(user_nth_pokemon) <= len(all_user_pokemons): 
+            nth_pokemon= all_user_pokemons[int(user_nth_pokemon) - 1]
+            return json.dumps(nth_pokemon)
+    return "No Pokemon captured!"
+
+
+#Allows a user to rename their pokemon
+@app.route('/mypokemon/<user_nth_pokemon>', methods=["PUT"])
+@jwt_required()
+def update_mypokemon(user_nth_pokemon):
+    data= request.get_json()
+    username= current_identity.username
+    user= User.query.filter_by(username= username).first()
+    if user:
+        user_id= user.id
+        all_user_pokemons= MyPokemon.query.filter_by(id= user_id)
+        all_user_pokemons= [pokemon for pokemon in all_user_pokemons]
+        if int(user_nth_pokemon) <= len(all_user_pokemons): 
+            nth_pokemon= all_user_pokemons[int(user_nth_pokemon) - 1]
+            nth_pokemon.pokemon.name= data['name']
+            db.session.add(nth_pokemon)
+            db.session.commit()
+            return "Updated"
+        return "No Pokemon found!"
+    return "Invalid User!"
+
+
+#Allows a user to release their pokemon
+@app.route('/mypokemon/<user_nth_pokemon>', methods=["DELETE"])
+@jwt_required()
+def delete_mypokemon(user_nth_pokemon):
+    username= current_identity.username
+    user= User.query.filter_by(username= username).first()
+    if user:
+        user_id= user.id
+        all_user_pokemons= MyPokemon.query.filter_by(id= user_id)
+        all_user_pokemons= [pokemon for pokemon in all_user_pokemons]
+        if int(user_nth_pokemon) <= len(all_user_pokemons): 
+            nth_pokemon= all_user_pokemons[int(user_nth_pokemon) - 1]
+            db.session.delete(nth_pokemon)
+            db.session.commit()
+            return "Deleted", 204
+        return "No Pokemon found!"
+    return "Invalid User!"
 
 
 
